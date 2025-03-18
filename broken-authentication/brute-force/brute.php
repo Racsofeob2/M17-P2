a9f1986d0f1a4c89636212f373312f9cb0bc296b
Racsofeob2 <108728179+Racsofeob2@users.noreply.github.com>
2025-03-18 12:26:39 +0100

    Update brute.php

--- broken-authentication/brute-force/brute.php
+++ broken-authentication/brute-force/brute.php
@@ -4,21 +4,16 @@ $db = new PDO('sqlite:users.db');
 $html= "";
 
 if(isset($_POST['username']) && isset($_POST['password'])  ){
-    $q = $db->prepare("SELECT * FROM users_ WHERE username=:user AND password=:pass");
-    $q->execute(array(
-        'user' => $_POST['username'],
-        'pass' => $_POST['password']
-    ));
-    $_select = $q -> fetch();
+    $q = $db->prepare("SELECT * FROM users_ WHERE username=:user");
+    $q->execute(array('user' => $_POST['username']));
+    $_select = $q->fetch();
 
-    if ( isset($_select['id'])) {
-
+    if ($_select && password_verify($_POST['password'], $_select['password'])) {
         session_start();
         $_SESSION['username'] = $_POST['username'];
-        $html =$strings["cong"];
-        //header("Location: index.php");
+        $html = $strings["cong"];
     } else {
-        $html=$strings["wrong"];
+        $html = $strings["wrong"];
     }
 
 }
