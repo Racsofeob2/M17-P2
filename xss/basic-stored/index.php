60f94111f5dce8a085bf65491b2cda7cbdc59da3
Racsofeob2 <108728179+Racsofeob2@users.noreply.github.com>
2025-03-18 13:30:51 +0100

    Update index.php

--- xss/basic-stored/index.php
+++ xss/basic-stored/index.php
@@ -43,29 +43,30 @@ if (isset($_POST['uname']) && isset($_POST['passwd'])) {
   <!-- Bootstrap CSS -->
   <link rel="stylesheet" type="text/css" href="bootstrap.min.css">
 
-  <title><?php echo $strings['title']; ?></title>
+  <title><?php echo htmlspecialchars($strings['title']); ?></title>
+
 </head>
 
 <body>
   <div class="container d-flex justify-content-center">
     <div class="shadow p-3 mb-5 rounded column" style="text-align: center; max-width: 1000px;margin-top:15vh;">
-      <h4>VULNLAB</h4>
+      <h4><?php echo htmlspecialchars('VULNLAB'); ?></h4>
 
       <form action="#" method="POST" style="text-align: center;margin-top: 20px;padding:30px;">
         <div class="row mb-3">
-          <label for="inputEmail3" class="col-sm-2 col-form-label">User</label>
+          <label for="inputEmail3" class="col-sm-2 col-form-label"><?php echo htmlspecialchars('User'); ?></label>
           <div class="col-sm-10">
             <input type="text" class="form-control" name="uname" id="inputEmail3">
           </div>
         </div>
         <div class="row mb-3">
-          <label for="inputPassword3" class="col-sm-2 col-form-label">Pass</label>
+<label for="inputPassword3" class="col-sm-2 col-form-label"><?php echo htmlspecialchars('Pass'); ?></label>
           <div class="col-sm-10">
             <input type="text" class="form-control" name="passwd" id="inputPassword3">
           </div>
         </div>
-        <button type="submit" class="btn btn-primary"><?php echo $strings['submit']; ?></button>
-        <p>mandalorian / mandalorian </p>
+<button type="submit" class="btn btn-primary"><?php echo htmlspecialchars($strings['submit']); ?></button>
+       <p><?php echo htmlspecialchars('mandalorian / mandalorian'); ?></p>
       </form>
 
 
